96570959d0cf90bae7e59edbe6d78d1bba7ea49b
Michael Heap <m@michaelheap.com>
2018-11-07 23:01:20 -0800

    Remove duplicate file upload method

--- ReplayService/server.py
+++ ReplayService/server.py
@@ -18,7 +18,7 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# used for uploading from the website
+@app.route("/upload", methods=["POST"])
 @app.route("/web_upload", methods=["POST"])
 def upload_file_web():
     # check if the post request has the file part
@@ -37,64 +37,40 @@ def upload_file_web():
         return response
     # passes
     if file and allowed_file(file.filename):
-        # save the file temporarily
-        tempfilename = secure_filename(file.filename)
-        tempfilepath = os.path.join(
-            app.config["UPLOAD_FOLDER"], str(time.time()) + tempfilename
-        )
 
-        file.save(tempfilepath)
-        # get the information from the replay
-        basic_json = process_rofl(tempfilepath)
-        # grab the match id and make a path using new file name
-        filename = str((json.loads(str(basic_json)))["MatchId"]) + ".rofl"
-        filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        if "match-id" in request.headers:
+            # If the user provided a match ID, use that name
+            matchid = request.headers["match-id"]
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], str(matchid) + ".rofl")
+            if not os.path.isfile(filepath):
+                file.save(filepath)
 
-        # check if the new file name exists, if not rename. Otherwise delete temporary
-        if not os.path.isfile(filepath):
-            os.rename(tempfilepath, filepath)
+            basic_json = process_rofl(filepath)
         else:
-            os.remove(tempfilepath)
+            # If the user didn't specify a match ID, read it from the provided file
+            tempfilename = secure_filename(file.filename)
+            tempfilepath = os.path.join(
+                app.config["UPLOAD_FOLDER"], str(time.time()) + tempfilename
+            )
+
+            file.save(tempfilepath)
+            # get the information from the replay
+            basic_json = process_rofl(tempfilepath)
+
+            # grab the match id and make a path using new file name
+            filename = str((json.loads(str(basic_json)))["MatchId"]) + ".rofl"
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+
+            # check if the new file name exists, if not rename. Otherwise delete temporary
+            if not os.path.isfile(filepath):
+                os.rename(tempfilepath, filepath)
+            else:
+                os.remove(tempfilepath)
 
         response = app.response_class(
             response=str(basic_json), status=200, mimetype="application/json"
         )
         return response
-
-
-# used for uploaded from desktop app
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        response = app.response_class(
-            response="no file", status=400, mimetype="text/plain"
-        )
-        return response
-    file = request.files["file"]
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == "":
-        response = app.response_class(
-            response="empty file", status=400, mimetype="text/plain"
-        )
-        return response
-    # passes
-    if file and allowed_file(file.filename):
-        matchid = request.headers["match-id"]
-        playername = request.headers["player-name"]
-        # check if file with matchid exists, it not save it
-        filepath = os.path.join(app.config["UPLOAD_FOLDER"], str(matchid) + ".rofl")
-        if not os.path.isfile(filepath):
-            file.save(filepath)
-
-        # get the information from the replay
-        basic_json = process_rofl(filepath)
-
-        response = app.response_class(
-            response=str(basic_json), status=200, mimetype="text/plain"
-        )
-        return response
-
 
 @app.route("/download/<string:matchid>", methods=["GET"])
 def download_file(matchid):
