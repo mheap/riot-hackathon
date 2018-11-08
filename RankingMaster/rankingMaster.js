9096c28a6edaa9066653e43e560c4b73fa2c9752
NatalieMok <nataliemok__@hotmail.com>
2018-11-08 14:54:54 -0800

    add getter for leaderboard

--- RankingMaster/rankingMaster.js
+++ RankingMaster/rankingMaster.js
@@ -62,6 +62,17 @@ app.get('/match', async (req, res) => {
   }
 });
 
+app.get('/leaderboard/:champ_id', (req, res) => {
+  const leaderboardData = await matchData.getLeaderboard(req.query.champ_id);
+
+  res.send(leaderboardData)
+});
+
+app.post('/evaluate', async (req, res) => {
+
+});
+
+
 app.listen(3000, '0.0.0.0', async () => {
   console.log(`Server listening on 3000`);
 
