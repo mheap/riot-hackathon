96158a42395d89ed57be8b24d91cade6e926c573
Michael Heap <m@michaelheap.com>
2018-11-07 21:21:58 -0800

    Add Dockerfile for Python server

--- ReplayService/server.py
+++ ReplayService/server.py
@@ -87,3 +87,6 @@ def download_file(matchid):
     else:
         response = app.response_class(response='not found',status=404,mimetype='text/plain')
         return response
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
