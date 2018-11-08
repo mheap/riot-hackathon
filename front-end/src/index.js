1e5ce97e095e582213d28af28b2dbbb3a7ae8d3f
Michael Heap <m@michaelheap.com>
2018-11-08 09:42:57 -0800

    Add React Router

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const ChampionPage = () => <div>Champion Page</div>
+
+const PrimaryLayout = () => (
+  <div className="primary-layout">
+    <main>
+      <Route path="/" exact component={App} />
+      <Route path="/:champion/leaderboard" component={ChampionPage} />
+    </main>
+  </div>
+)
+
+const Base = () => (
+  <BrowserRouter>
+    <PrimaryLayout />
+  </BrowserRouter>
+)
+
+ReactDOM.render(<Base />, document.getElementById('root'))
