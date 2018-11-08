50e716ac12590c4938e70b84d8060f669d4b6f15
Michael Heap <m@michaelheap.com>
2018-11-08 13:55:10 -0800

    Add drag and stop file uploads

--- front-end/src/SelectChampion.js
+++ front-end/src/SelectChampion.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { GridGenerator, HexGrid, Layout, Path, Hexagon, Text, Pattern, Hex } from 'react-hexgrid';
+import { GridGenerator, HexGrid, Layout, Hexagon, Text, Pattern } from 'react-hexgrid';
+import ReactDropzone from "react-dropzone";
+import request from "superagent";
+
 import './App.css';
 import {champions, profileImage} from './champion';
 
@@ -7,6 +10,28 @@ export default class SelectChampion extends Component {
 
     constructor(props) {
       super(props);
+        this.uploadText = 'Drop your replay here'
+    }
+
+    onDrop = (files) => {
+
+        if (files.length > 1) {
+            alert("Please upload one file at a time");
+            return;
+        }
+
+        // POST to a test endpoint for demo purposes
+        const req = request.post('http://localhost:5000/upload');
+
+        files.forEach(file => {
+            req.attach("file", file);
+        });
+
+        req.then((data) => {
+            this.setState((state, props) => {
+                this.uploadText = `<a href="/match/${data.body.matchId}">Match ${data.body.matchId} uploaded</a>`;
+            });
+        }).catch(console.log);
     }
 
     render() {
@@ -17,6 +42,11 @@ export default class SelectChampion extends Component {
           <div className="plate">
             <p className="shadow text1">UPLOAD</p>
             <p className="shadow text2">ROFL</p>
+          </div>
+          <div className="upload-button">
+          <ReactDropzone disableClick onDrop={this.onDrop}>
+          <p dangerouslySetInnerHTML={{__html: this.uploadText}} />
+          </ReactDropzone>
           </div>
           <HexGrid width={1200} height={1250}>
 
@@ -29,8 +59,8 @@ export default class SelectChampion extends Component {
                     }
 
                     return (
-                    <a href={link}>
-                      <Hexagon fill={i.toString()} key={i} q={hex.q} r={hex.r} s={hex.s}>
+                    <a key={i.toString() + 'link'} href={link}>
+                      <Hexagon fill={i.toString()} key={i.toString()} q={hex.q} r={hex.r} s={hex.s}>
                         <Text>{champions[i]}</Text>
                       </Hexagon>
                     </a>
@@ -41,7 +71,7 @@ export default class SelectChampion extends Component {
 
             {
               champions.map((champion, i) => {
-                  return <Pattern id={i} link={profileImage(champion)} size={{ x: 4, y: 4 }} />
+                  return <Pattern key={i.toString()} id={i.toString()} link={profileImage(champion)} size={{ x: 4, y: 4 }} />
               })
             }
           </HexGrid>
