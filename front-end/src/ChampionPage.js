baf49e3d04e0f2352811df97de06c98695db987a
Konrad Cheng <konrad.see.jung.cheng@ibm.com>
2018-11-08 10:34:20 -0800

    champion clicking passes in champion name

--- front-end/src/ChampionPage.js
+++ front-end/src/ChampionPage.js
@@ -6,12 +6,12 @@ export default class ChampionPage extends Component {
 
     constructor(props) {
       super(props);
+      console.log(props);
     }
 
     render() {
-
       return (
-        <h1>Hello</h1>
+        <h1>{this.props.match.params.champion}</h1>
       );
     }
 }
