b93cde3821a4883481b9926b0dfc10d423745de3
Konrad Cheng <konrad.see.jung.cheng@ibm.com>
2018-11-09 11:46:05 -0800

    slight changes to match page

--- front-end/src/MatchPage.js
+++ front-end/src/MatchPage.js
@@ -75,7 +75,7 @@ export default class MatchPage extends Component {
                     </div>
                     <div>
                       <div className="shadow text1 matchsummonername">{localStorage.getItem('summonerName')}</div>
-                      <div>Rank "Number" {champion.name}</div>
+                      <div>Rank TBD {champion.name}</div>
                     </div>
                   </div>
                   <div className="matchpageleftbox">
